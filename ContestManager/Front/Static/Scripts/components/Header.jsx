7a63e654312b953732e3f168b1c7472c2639192f
Дубровин Алексей <a.dubrovin@skbkontur.ru>
2018-06-14 10:27:34 +0500

    render fix

--- ContestManager/Front/Static/Scripts/components/Header.jsx
+++ ContestManager/Front/Static/Scripts/components/Header.jsx
@@ -4,38 +4,24 @@ import {LinkContainer} from "react-router-bootstrap";
 import UserRole from '../Common/UserRole';
 
 class Header extends Navbar {
-    constructor(props, context) {
-        super(props, context);
-
-        this.state = {
-            user: null,
-        };
-    }
-
-    componentDidMount() {
-        this.getUser();
-    }
-
     logOut = () => {
         this.props.cookies.remove("CM-User");
-        this.getUser();
         this.props.history.push('/');
+        this.render();
     };
 
     getUser = () => {
         const str = this.props.cookies.get("CM-User");
+        let user = null;
         if (str) {
             const splitted = str.split('&');
-            this.setState({
-                user: {
-                    name: splitted[1].split('=')[1],
-                    role: splitted[2].split('=')[1],
-                }
-            });
+            user = {
+                name: splitted[1].split('=')[1],
+                role: splitted[2].split('=')[1],
+            };
         }
-        else {
-            this.setState({user: null});
-        }
+
+        return user;
     };
 
     getUserSection = () => {
@@ -43,11 +29,13 @@ class Header extends Navbar {
             fontWeight: 'bold'
         };
 
-        if (this.state.user)
+        const user = this.getUser();
+
+        if (user)
             return (
                 <Nav pullRight>
-                    <Navbar.Text style={nameStyle}> {this.state.user.name} </Navbar.Text>
-                    {UserRole[this.state.user.role] & UserRole.ContestManager
+                    <Navbar.Text style={nameStyle}> {user.name} </Navbar.Text>
+                    {UserRole[user.role] & UserRole.ContestManager
                         ?
                         (<LinkContainer to="/contests/create">
                             <NavItem> Создать контест </NavItem>
