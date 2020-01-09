d5385f60d95160885c84494afa70f5defc96c813
Дубровин Алексей <a.dubrovin@skbkontur.ru>
2020-01-09 11:16:07 +0300

    validation on editableuser

--- ContestManager/Front.React/ClientApp/src/components/UserPage/EditableUserData.jsx
+++ ContestManager/Front.React/ClientApp/src/components/UserPage/EditableUserData.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Button, Col, Form, FormGroup, Input, Label } from 'reactstrap';
+import { Alert, Button, Col, Label } from 'reactstrap';
 import { UserRole } from '../../Enums/UserRole';
 import { patch } from '../../Proxy';
 import withUser from '../HOC/WithUser';
+import { AvForm, AvGroup, AvInput } from 'availity-reactstrap-validation';
 
 class EditableUserData extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
             user: props.user,
+            hasErrors: false,
         };
+
         this.save = this.save.bind(this);
     }
 
-    async save() {
+    async save(event, errors) {
+        if (errors.length) {
+            this.setState({ hasErrors: true });
+            return false;
+        }
         await patch('users', this.state.user);
         this.props.setUser(this.state.user);
         if (this.props.onSave) {
@@ -25,12 +32,21 @@ class EditableUserData extends React.Component {
         const { target } = event;
         const { name } = target;
         let value = target.value;
-        if (name === 'role' || name === 'sex')
+        if (name === 'role' || name === 'sex' || name === 'class')
             value = parseInt(value, 10);
+        if (name === 'sex')
+            value--;
 
         await this.setState({
-            user: { ...this.state.user, [name]: value }
+            user: {
+                ...this.state.user, [name]: value,
+            },
+            hasErrors: false
         });
+    };
+
+    validateSelect = (value, ctx, input, cb) => {
+        return value !== -1;
     };
 
     render() {
@@ -39,26 +55,38 @@ class EditableUserData extends React.Component {
         const smLabel = small ? 4 : 1;
         const smOffset = small ? 4 : 2;
         const smInput = small ? 8 : 4;
-        return (<Form>
-            <FormGroup row>
+        return (<AvForm onSubmit={this.save}>
+            <AvGroup row>
                 <Label sm={smLabel}>Имя</Label>
                 <Col sm={smInput}>
-                    <Input type="text" name="name" onChange={this.handleChange} value={name} />
+                    <AvInput type="text" name="name" onChange={this.handleChange} value={name} required />
                 </Col>
-            </FormGroup>
-            {role === UserRole.Participant && <FormGroup row>
+            </AvGroup>
+            {role === UserRole.Participant && <AvGroup row>
                 <Label sm={smLabel}>Пол</Label>
                 <Col sm={smInput}>
-                    <Input type="select" name="sex" onChange={this.handleChange} value={sex}>
-                        <option value={0}>М</option>
-                        <option value={1}>Ж</option>
-                    </Input>
+                    <AvInput type="select"
+                             name="sex"
+                             onChange={this.handleChange}
+                             value={sex !== null ? sex + 1 : -1}
+                             validate={{ async: this.validateSelect }}
+                             required>
+                        <option value={-1}>Не выбран</option>
+                        <option value={1}>М</option>
+                        <option value={2}>Ж</option>
+                    </AvInput>
                 </Col>
-            </FormGroup>}
-            {role === UserRole.Participant && <FormGroup row>
+            </AvGroup>}
+            {role === UserRole.Participant && <AvGroup row>
                 <Label sm={smLabel}>Класс</Label>
                 <Col sm={smInput}>
-                    <Input type="select" name="class" onChange={this.handleChange} value={stateClass}>
+                    <AvInput type="select"
+                             name="class"
+                             onChange={this.handleChange}
+                             value={stateClass !== null ? stateClass : -1}
+                             validate={{ async: this.validateSelect }}
+                             required>
+                        <option value={-1}>Не выбран</option>
                         <option value={5}>5</option>
                         <option value={6}>6</option>
                         <option value={7}>7</option>
@@ -66,51 +94,59 @@ class EditableUserData extends React.Component {
                         <option value={9}>9</option>
                         <option value={10}>10</option>
                         <option value={11}>11</option>
-                    </Input>
+                    </AvInput>
                 </Col>
-            </FormGroup>}
+            </AvGroup>}
             {(role === UserRole.Participant || role === UserRole.Coach) &&
-            <FormGroup row>
+            <AvGroup row>
                 <Label sm={smLabel}>Город</Label>
                 <Col sm={smInput}>
-                    <Input type="text" name="city" onChange={this.handleChange} value={city} />
+                    <AvInput type="text" name="city" onChange={this.handleChange} value={city} required />
                 </Col>
-            </FormGroup>}
+            </AvGroup>}
             {(role === UserRole.Participant || role === UserRole.Coach) &&
-            <FormGroup row>
+            <AvGroup row>
                 <Label sm={smLabel}>Школа</Label>
                 <Col sm={smInput}>
-                    <Input type="text"
-                           name="school"
-                           onChange={this.handleChange}
-                           value={school} />
+                    <AvInput type="text"
+                             name="school"
+                             onChange={this.handleChange}
+                             value={school}
+                             required />
                 </Col>
-            </FormGroup>}
-            {role === UserRole.Participant && <FormGroup row>
+            </AvGroup>}
+            {role === UserRole.Participant && <AvGroup row>
                 <Label sm={smLabel}>Тренер</Label>
                 <Col sm={smInput}>
-                    <Input type="text"
-                           name="coach"
-                           onChange={this.handleChange}
-                           value={coach} />
+                    <AvInput type="text"
+                             name="coach"
+                             onChange={this.handleChange}
+                             value={coach} />
                 </Col>
-            </FormGroup>}
-            {!small && <FormGroup row>
+            </AvGroup>}
+            {!small && <AvGroup row>
                 <Label sm={smLabel}>Роль</Label>
                 <Col sm={smInput}>
-                    <Input type="select" name="role" onChange={this.handleChange} value={role}>
+                    <AvInput type="select" name="role" onChange={this.handleChange} value={role} required>
                         <option value={1}>Участник</option>
                         <option value={2}>Тренер</option>
-                    </Input>
+                    </AvInput>
                 </Col>
-            </FormGroup>}
+            </AvGroup>}
             {this.props.children}
-            <FormGroup row>
+            {this.state.hasErrors && (
+                <AvGroup row>
+                    <Col sm={smInput + smLabel}>
+                        <Alert color="danger">Исправьте ошибки в данных</Alert>
+                    </Col>
+                </AvGroup>
+            )}
+            <AvGroup row>
                 <Col sm={{ offset: smOffset }}>
-                    <Button onClick={this.save}>{this.props.saveTitle || 'Сохранить'}</Button>
+                    <Button>{this.props.saveTitle || 'Сохранить'}</Button>
                 </Col>
-            </FormGroup>
-        </Form>);
+            </AvGroup>
+        </AvForm>);
     }
 }
 
