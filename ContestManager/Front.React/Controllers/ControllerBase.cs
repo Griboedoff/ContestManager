11168779b4b17e9f3e056cdf1a38be244e2f7f31
Дубровин Алексей <a.dubrovin@skbkontur.ru>
2020-01-09 07:48:29 +0300

    nocache

--- ContestManager/Front.React/Controllers/ControllerBase.cs
+++ ContestManager/Front.React/Controllers/ControllerBase.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Front.React.Controllers
 {
     [Route("api/[controller]")]
+    [ResponseCache(NoStore = true, Duration = 0)]
     public class ControllerBase : Controller
     {
     }
