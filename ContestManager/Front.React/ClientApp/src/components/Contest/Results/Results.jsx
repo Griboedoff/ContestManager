da0cb3392049d112769062b113c19bbdfd345fc7
Дубровин Алексей <a.dubrovin@skbkontur.ru>
2020-02-06 15:52:42 +0300

    Фильтрую чуваков без результатов

--- ContestManager/Front.React/ClientApp/src/components/Contest/Results/Results.jsx
+++ ContestManager/Front.React/ClientApp/src/components/Contest/Results/Results.jsx
@@ -10,12 +10,13 @@ const Results = ({ match, participants }) => {
     const [currentClass, setCurrentClass] = useState(defaultClass);
     const tasksCount = useMemo(() => Math.max(...participants.map(p => p.results.length)), [participants]);
     const filteredParticipants = useMemo(() => sortBy(
-        participants.filter(p => p.userSnapshot.class === currentClass),
+        participants.filter(p => p.userSnapshot.class === currentClass && p.results.length !== 0),
         [p => -p.results.map(r => parseInt(r)).reduce((a, b) => a + b, 0), p => p.userSnapshot.name]
         ),
         [participants, currentClass]);
 
     const hasParticipants = filteredParticipants.length !== 0;
+    const noResults = [...Array(tasksCount).keys()].map(_ => 0);
 
     return <Container>
         <h4 className="mt-3">Результаты</h4>
@@ -35,33 +36,33 @@ const Results = ({ match, participants }) => {
         </Nav>
 
         {hasParticipants ? <Table bordered size="sm">
-            <thead>
-            <tr>
-                <th rowSpan="2">Участник</th>
-                <th rowSpan="2" style={{ width: "20%" }}>Учебное заведение</th>
-                <th colSpan={tasksCount}>Задача</th>
-                <th rowSpan="2">Сумма</th>
-                <th rowSpan="2">Место</th>
-            </tr>
-            <tr>
-                {[...Array(tasksCount).keys()].map((_, n) => <td key={n}>{n + 1}</td>)}
-            </tr>
-            </thead>
-            <tbody>
-            {filteredParticipants.map(p => {
-                const user = p.userSnapshot;
+                <thead>
+                <tr>
+                    <th rowSpan="2">Участник</th>
+                    <th rowSpan="2" style={{ width: "20%" }}>Учебное заведение</th>
+                    <th colSpan={tasksCount}>Задача</th>
+                    <th rowSpan="2">Сумма</th>
+                    <th rowSpan="2">Место</th>
+                </tr>
+                <tr>
+                    {[...Array(tasksCount).keys()].map((_, n) => <td key={n}>{n + 1}</td>)}
+                </tr>
+                </thead>
+                <tbody>
+                {filteredParticipants.map(p => {
+                    const user = p.userSnapshot;
 
-                return <tr key={p.id}>
-                    <td>{user.name}</td>
-                    <td>{`${user.school} (${user.city})`}</td>
-                    {p.results.map((r, i) => <td key={i}>{r}</td>)}
-                    <td>{p.results.reduce((a, b) => parseInt(b) + a, 0)}</td>
-                    <td>{p.place || ""}</td>
-                </tr>;
-            })}
-            </tbody>
-        </Table>:
-        <Alert color="secondary">Нет участников в {currentClass} классе</Alert>}
+                    return <tr key={p.id}>
+                        <td>{user.name}</td>
+                        <td>{`${user.school} (${user.city})`}</td>
+                        {p.results.map((r, i) => <td key={i}>{r}</td>)}
+                        <td>{p.results.reduce((a, b) => parseInt(b) + a, 0)}</td>
+                        <td>{p.place || ""}</td>
+                    </tr>;
+                })}
+                </tbody>
+            </Table> :
+            <Alert color="secondary">Нет участников в {currentClass} классе</Alert>}
     </Container>;
 };
 
